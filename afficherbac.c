6a8c83c82340ae7bb9bd5b12d5bc0a9e320d3dec
gibran861 <80194273+gibran861@users.noreply.github.com>
2021-03-29 23:34:40 +0200

    Delete afficherbac.c

--- afficherbac.c
+++
@@ -1,5 +0,0 @@
-void aficherBack(Background b, SDL_Surface * screen);
-{SDL_BlitSurface(image1, NULL, screen, &positionecran);
-SDL_BlitSurface(perso, NULL, screen, &positionperso);
-SDL_BlitSurface(b.vie, NULL, screen, &position_vie);
-SDL_Flip(screen);
